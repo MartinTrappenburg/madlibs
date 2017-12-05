f6e9c759f47894d7b847d68e57638a7266da1322
ASSASSINS-CREED\Martin <martintrap1@gmail.com>
2017-12-05 12:14:51 +0100

    initial
    system first. styling later

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MadLibs</title>
+		<link rel="stylesheet" type="text/css" href="IndexCSS.css">
+	</head>
+
+	<body>
+		<span id="Container">
+			<div id="box">
+				<form method="post" action="">
+ 					<p>Welk dier zou je nooit als huisdier willen?</p><input type="" name="huisdier">
+ 					<p>Wie is de belangrijkste persoon in je leven?</p><input type="" name="persoon">
+ 					<p>In welk land zou je graag willen wonen</p><input type="" name="land">
+ 					<p>Wat doe je als je je verveelt?</p><input type="" name="verveling">
+ 					<p>Met welk speelgoed speelde je als kind het meest?</p><input type="" name="speelgoed">
+ 					<p>Bij welke docent spijbel je het liefst?</p><input type="" name="spijbelen">
+ 					<p>Als je €100.000,- had, wat zou je dan kopen?</p><input type="" name="geld">
+ 					<p>Wat is je favoriete bezigheid?</p><input type="" name="bezigheid">
+				</form>
+			</div>
+		</span>
+	</body>
+</html>	
